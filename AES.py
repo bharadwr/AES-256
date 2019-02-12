d15ed43d38cf283a02c76ba749c935b9c2856168
Rtvik Bharadwaj <bharadwr@purdue.edu>
2019-02-11 22:33:02 -0500

    Adding g() func and keygen

--- AES.py
+++ AES.py
@@ -11,3 +11,43 @@ AES_modulus = BitVector(bitstring='100011011')
 
 subBytesTable = []
 invSubBytesTable = []
+
+def gen_key_schedule_256(key_bv):
+    byte_sub_table = gen_subbytes_table()
+    #  We need 60 keywords (each keyword consists of 32 bits) in the key schedule for
+    #  256 bit AES. The 256-bit AES uses the first four keywords to xor the input
+    #  block with.  Subsequently, each of the 14 rounds uses 4 keywords from the key
+    #  schedule. We will store all 60 keywords in the following list:
+    key_words = [None for i in range(60)]
+    round_constant = BitVector(intVal = 0x01, size=8)
+    for i in range(8):
+        key_words[i] = key_bv[i*32 : i*32 + 32]
+    for i in range(8,60):
+        if i%8 == 0:
+            kwd, round_constant = gee(key_words[i-1], round_constant, byte_sub_table)
+            key_words[i] = key_words[i-8] ^ kwd
+        elif (i - (i//8)*8) < 4:
+            key_words[i] = key_words[i-8] ^ key_words[i-1]
+        elif (i - (i//8)*8) == 4:
+            key_words[i] = BitVector(size = 0)
+            for j in range(4):
+                key_words[i] += BitVector(intVal =
+                                 byte_sub_table[key_words[i-1][8*j:8*j+8].intValue()], size = 8)
+            key_words[i] ^= key_words[i-8]
+        elif ((i - (i//8)*8) > 4) and ((i - (i//8)*8) < 8):
+            key_words[i] = key_words[i-8] ^ key_words[i-1]
+        else:
+            sys.exit("error in key scheduling algo for i = %d" % i)
+    return key_words
+
+
+def gee(keyword, round_num_constant, byte_sub_table):
+    rotated_word = keyword.deep_copy()
+    rotated_word << 8
+    newword = BitVector(size=0)
+    for i in range(4):
+        newword += BitVector(intVal=byte_sub_table[rotated_word[8 * i:8 * i + 8].intValue()], size=8)
+    newword[:8] ^= round_num_constant
+    round_num_constant = round_num_constant.gf_multiply_modular(BitVector(intVal=0x02), AES_modulus, 8)
+    return newword, round_num_constant
+
